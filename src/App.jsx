33120b8ef20dad7eb4019272bbb634fb6535a8ab
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-18 12:00:05 +0530

    className="grid grid-rows-[auto_1fr_auto]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 function App() {
   return (
-    <section className="text-center text-emerald-700">
+    <section className="text-center text-emerald-700 grid h-screen grid-rows-[auto_1fr_auto]">
       <nav className="bg-yellow-500 py-2 md:p-4 text-sm sm:text-base md:text-[30px] ">
         Main Navigation
       </nav>
 
-      <main>
+      <main className="bg-blue-400">
         <div className="bg-slate-200 text-3xl text-blue-500 py-4 my-8 border-b-4 border-blue-600">
           <h1>Left Side</h1>
         </div>
@@ -32,7 +32,22 @@ function App() {
           <div>Item</div>
           <div>Item</div>
         </div>
+
+        <article className="flex justify-between items-center px-4 py-4 sm:px-8 mt-10 bg-orange-300">
+          <div className="flex gap-1">
+            <p className="font-bold">Cart Item:</p>
+            <p>Soccer Shoe</p>
+          </div>
+
+          <div>
+            <p>Total: $45</p>
+          </div>
+        </article>
       </main>
+
+      <footer className="h-[100px] bg-slate-600 text-white flex flex-col justify-center">
+        <p>&copy; Emmeisoft Pvt. Ltd.</p>
+      </footer>
     </section>
   )
 }
