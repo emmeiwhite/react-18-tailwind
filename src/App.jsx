44647a9f63bf924b1539121059ff51667e6ecb74
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-18 10:45:47 +0530

    text-color and bg-color

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 function App() {
   return (
-    <>
-      <nav>Main Navigation</nav>
+    <section className="text-center text-emerald-700">
+      <nav className="bg-yellow-500">Main Navigation</nav>
 
       <main>
-        <div className="movies-wrapper">
+        <div className="bg-slate-400">
           <h1>Left Side</h1>
         </div>
-        <div className="movies-details">
+        <div className="bg-stone-300">
           <h1>Right Side</h1>
         </div>
       </main>
-    </>
+    </section>
   )
 }
 
