8a1c74db5ed86321f8c1fefcfd63fcc44f9a2a94
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-18 11:04:15 +0530

    tracking (letter spacing) bg-slate-100, text-semibold

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ function App() {
       <nav className="bg-yellow-500">Main Navigation</nav>
 
       <main>
-        <div className="bg-slate-400">
+        <div className="bg-slate-200 text-3xl text-blue-500">
           <h1>Left Side</h1>
         </div>
         <div className="bg-stone-300">
-          <h1>Right Side</h1>
+          <h1 className="font-semibold uppercase tracking-[0.5rem]">Right Side</h1>
+          <p className="text-semibold tracking-widest">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, recusandae debitis.
+            Minima tempore, quibusdam unde voluptas veritatis quod rerum dicta corporis fugiat,
+            perspiciatis dolore distinctio magnam, nemo consequuntur. Quidem, facere.
+          </p>
         </div>
       </main>
     </section>
