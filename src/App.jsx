2cceebe1909e5f12c145d2e8a412a32f7edd938c
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-18 10:34:47 +0530

    Revisiting Tailwind before going forward with the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <nav>Main Navigation</nav>
+
+      <main>
+        <div className="movies-wrapper">
+          <h1>Left Side</h1>
+        </div>
+        <div className="movies-details">
+          <h1>Right Side</h1>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
